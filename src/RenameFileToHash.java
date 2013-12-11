3a6fd44a5e2fb047bc30b690a5d9b5ae5244a50a
cut-up <unclean@i2pmail.org>
2013-12-11 04:58:01 +0400

    debug

--- src/RenameFileToHash.java
+++ src/RenameFileToHash.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RenameFileToHash {
     private final Scanner scanner;
-    private int advance;
     private File[] listFiles;
     private MessageDigest messageDigest;
+    private int radix, numberMask, number, change;
 
     private RenameFileToHash() {
         this.scanner = new Scanner(System.in);
@@ -32,16 +33,17 @@ public class RenameFileToHash {
                 System.out.printf("%s is renamed to %s.\n", oldFile.getName(), newFile.getName());
             }
         }
-        System.out.printf("%s of %s files renamed.\n", renameToHash.getAdvance(), renameToHash.getListFiles().length);
+        System.out.printf("%s of %s files renamed.\n", renameToHash.getChange(), renameToHash.getListFiles().length);
     }
 
-    public int getAdvance() {
-        return this.advance;
+    public int getChange() {
+        return this.change;
     }
 
     public String getHash(File file) {
         final byte[] bytes = new byte[1024];
         FileInputStream fileInputStream = null;
+        String hash;
         try {
             fileInputStream = new FileInputStream(file.getPath());
             for (int read; (read = fileInputStream.read(bytes)) != -1; ) {
@@ -58,7 +60,11 @@ public class RenameFileToHash {
                 }
             }
         }
-        return new BigInteger(1, messageDigest.digest()).toString(16);
+        hash = new BigInteger(1, messageDigest.digest()).toString(16);
+        if (hash.length() != this.radix) {
+            hash = String.format("%0" + (this.radix - hash.length()) + "d%s", 0, hash);
+        }
+        return hash;
     }
 
     public File[] getListFiles() {
@@ -66,11 +72,16 @@ public class RenameFileToHash {
     }
 
     public File getNewFile(File file, String hash) {
-        String newName = file.getPath().substring(0, file.getPath().lastIndexOf(file.getName())) + hash;
-        if (file.getName().lastIndexOf(".") != -1) {
-            return new File(newName + file.getName().substring(file.getName().lastIndexOf(".")));
+        StringBuilder newName = new StringBuilder();
+        newName.append(file.getPath().substring(0, file.getPath().lastIndexOf(file.getName())));
+        if (this.numberMask != 0) {
+            newName.append(String.format("%0" + this.numberMask + "d-", ++this.number));
         }
-        return new File(newName);
+        newName.append(hash);
+        if (file.getName().lastIndexOf(".") != -1) {
+            return new File(newName.append(file.getName().substring(file.getName().lastIndexOf(".")).toLowerCase()).toString());
+        }
+        return new File(newName.toString());
     }
 
     public boolean isDuplicate(File oldName, File newName) {
@@ -80,7 +91,7 @@ public class RenameFileToHash {
             }
             return true;
         }
-        this.advance++;
+        this.change++;
         return false;
     }
 
@@ -100,10 +111,11 @@ public class RenameFileToHash {
                 }
             }
         }
+        this.radix = messageDigest.digest().length * 2;
     }
 
-    private void setListFiles(String[] args) {
-        File path = args.length != 0 ? new File(args[0]) : new File(".");
+    private void setListFiles(String[] arguments) {
+        File path = arguments.length != 0 ? new File(arguments[0]) : new File(".");
         while (!path.isFile() && !path.isDirectory()) {
             System.out.printf("No such file or directory.\nPath: ");
             path = new File(scanner.nextLine());
@@ -125,6 +137,8 @@ public class RenameFileToHash {
                     return false;
                 }
             });
+            Arrays.sort(this.listFiles);
+            this.numberMask = ("" + this.listFiles.length).length();
         }
     }
 }
